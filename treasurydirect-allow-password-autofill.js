ce565e07fd5dbb2fee72c86cffafb96b26718853
Weston Reed <weston.reed@he.net>
2023-04-01 21:43:38 -0700

    Added script

---
+++ treasurydirect-allow-password-autofill.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Treasury Direct Allow Password Autofill
+// @namespace    https://www.treasurydirect.gov/*
+// @version      0.1
+// @description  Removes the readonly attribute on treasurydirect.gov so that password managers can autofill the input.
+// @author       Weston Reed
+// @match        https://www.treasurydirect.gov/RS/PW-Display.do*
+// @icon         https://www.google.com/s2/favicons?domain=treasurydirect.gov
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.getElementsByClassName('pwordinput')[0].removeAttribute('readonly');
+})();
